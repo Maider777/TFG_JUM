1b195f0b0485f5bd1f355c943f983abb6080e8b7
Deagle50 <urkourbieta@gmail.com>
2022-05-13 09:27:53 +0200

    Corregido obtener usuario

--- api/src/objetos/usuarios.js
+++ api/src/objetos/usuarios.js
@@ -5,8 +5,8 @@ const config = require("../db/dbconfig"),
 async function obtenerUsuario(usuario) {
   try {
     let pool = await sql.connect(config);
-    let usuario = await pool.request().query(`SELECT ${db.CAMPOS_USUARIOS} FROM ${db.TABLAS.USUARIOS} WHERE usuario = '${usuario}'`);
-    return usuario.recordsets[0];
+    let user = await pool.request().query(`SELECT ${db.CAMPOS_USUARIOS} FROM ${db.TABLAS.USUARIOS} WHERE usuario = '${usuario}'`);
+    return user.recordsets[0];
   } catch (error) {
     return error;
   }
